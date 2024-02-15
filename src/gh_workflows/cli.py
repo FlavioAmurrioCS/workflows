c03dca4c2ec524c2e7668274fe2fe8c9f5287f3e
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2024-02-15 12:52:48 -0500

    added hatch package

--- src/gh_workflows/cli.py
+++ src/gh_workflows/cli.py
@@ -6,11 +6,11 @@ from typing import Sequence
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
+    parser.add_argument("filename")
     args = parser.parse_args(argv)
     print(args)
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
