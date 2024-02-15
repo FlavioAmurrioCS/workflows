95c2606933dc491a9724f9f73aad1149c4a372a9
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2024-02-15 10:39:50 -0500

    Testing testing

---
+++ workflows.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import argparse
+from typing import Sequence
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    args = parser.parse_args(argv)
+    print(args)
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
